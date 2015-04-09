f9533d989f2079452603f8a70c4fe6906f556ead
silentcharacter <2210qwer>
2015-04-09 15:47:13 +0300

    Клиент солр

--- src/main/java/SolrClient.java
+++ src/main/java/SolrClient.java
@@ -3,7 +3,9 @@ import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.common.SolrInputDocument;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,6 +17,7 @@ public class SolrClient {
         // create the SolrJ client
         HttpSolrServer client = new HttpSolrServer("http://localhost:8983/solr/master_tn_Product");
 
+        List<SolrInputDocument> docs = new ArrayList<>();
         // create the document
         SolrInputDocument sdoc = new SolrInputDocument();
         sdoc.addField("id","book1");
@@ -27,9 +30,20 @@ public class SolrClient {
         fieldModifier.put("set", "sdfsdf");
         sdoc.addField("author_string", fieldModifier);  // add the map as the field value
 
-//        sdoc.removeField("cat_string");
+        Map<String,Object> fieldModifier1 = new HashMap<>(1);
+        fieldModifier1.put("inc", "1");
+        sdoc.addField("copies_double", fieldModifier1);  // add the map as the field value
 
-        client.add( sdoc );  // send it to the solr server
+//        sdoc.removeField("cat_string");
+        docs.add(sdoc);
+
+        SolrInputDocument sdoc1 = new SolrInputDocument();
+        sdoc1.addField("id","book2");
+        sdoc1.addField("test_string", "test");
+        docs.add(sdoc1);
+
+        client.add(docs);
+
         client.commit();
         client.shutdown();  // shutdown client before we exit
     }
